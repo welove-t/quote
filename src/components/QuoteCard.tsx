fe0ca7edc2438068ed48f159cc07bde1c50117f0
welove-t <tlove.tky@gmail.com>
2022-05-12 11:38:50 +0900

    💄 BlockQuoteに設定色反映(文字)

--- src/components/QuoteCard.tsx
+++ src/components/QuoteCard.tsx
@@ -3,20 +3,24 @@ import { Blockquote } from "@mantine/core";
 
 type props = {
   quote: string;
-  container: null | LegacyRef<HTMLImageElement>;
-  png: string | null;
-  width: number;
-  height: number;
+  bgColor: string;
+  foColor: string;
 };
 
-const QuoteCard = ({ quote, container, png, width, height }: props) => {
+const QuoteCard = ({ quote, bgColor, foColor }: props) => {
   return (
-    <div className="text-center">
-      <div className="my-20 mx-auto mb-4 flex items-center border-solid sm:h-52">
-        <Blockquote cite="– Forrest Gump" id="canvas">
-          <pre>{quote}</pre>
-        </Blockquote>
-      </div>
+    <div className="my-20 mx-auto mb-4 flex items-center border-solid text-center sm:h-52">
+      <Blockquote
+        cite="– Forrest Gump"
+        id="canvas"
+        styles={{
+          icon: { color: foColor },
+          inner: { color: foColor },
+          body: { color: foColor },
+        }}
+      >
+        <pre>{quote}</pre>
+      </Blockquote>
     </div>
   );
 };
