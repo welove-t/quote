d684891f21855826abd2379e253fde63e94ff7c4
welove-t <tlove.tky@gmail.com>
2022-06-15 11:09:29 +0900

    💄 ヘッダのスタイル調整

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,11 +4,14 @@ type props = {
   themeColor: string;
 };
 
-const Header = ({ themeColor }: props) => {
+const Header = ({ themeColor = "#F3F3F3" }: props) => {
   return (
-    <div style={{ backgroundColor: themeColor }}>
-      <header className="container m-0 flex h-16 items-center">
-        <span className="text-lg font-semibold text-gray-500">Quote Card</span>
+    <div
+      style={{ backgroundColor: themeColor }}
+      className="fixed -inset-x-0 top-0 left-0 z-50 border-b border-solid border-gray-200"
+    >
+      <header className="container flex h-16 items-center justify-center">
+        <span className="text-2xl font-semibold text-gray-500">Quote Card</span>
       </header>
     </div>
   );
