2f2ae7083184f39e6954ea48d9c89561ae0dbe16
welove-t <tlove.tky@gmail.com>
2022-06-18 13:17:31 +0900

    ✨ ダークモードの機能実装

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,22 @@
+import { useMantineTheme } from "@mantine/core";
+import { useColorScheme } from "@mantine/hooks";
 import React from "react";
+import { ColorTheme } from "src/components/ColorTheme";
 
 type props = {
   themeColor: string;
 };
 
-const Header = ({ themeColor = "#F3F3F3" }: props) => {
+const Header = ({ themeColor }: props) => {
   return (
     <div
       style={{ backgroundColor: themeColor }}
       className="fixed -inset-x-0 top-0 left-0 z-50 border-b border-solid border-gray-200"
     >
-      <header className="container flex h-16 items-center justify-center">
+      <header className="container flex h-16 items-center justify-center space-x-8">
         <span className="text-2xl font-semibold text-gray-500">Quote Card</span>
       </header>
+      <ColorTheme />
     </div>
   );
 };
