795f3137a000f6872a9eb01956571899c96221ff
welove-t <tlove.tky@gmail.com>
2022-06-20 14:41:44 +0900

    🐛 カードシェア時に左寄りになる現象を解消

--- src/components/QuoteCard.tsx
+++ src/components/QuoteCard.tsx
@@ -10,17 +10,17 @@ type props = {
 const QuoteCard = ({ themeColor, quote, source }: props) => {
   return (
     <div
-      className={`max-w-[360px] rounded-xl p-2 text-center sm:inline-block sm:w-[360px] ${themeColor}`}
+      className={`max-w-[360px] p-2 text-center sm:inline-block sm:w-[360px] ${themeColor}`}
       id="canvas"
     >
       <div
-        className={`relative my-1 flex min-h-[320px] flex-col items-center justify-center whitespace-pre-wrap rounded-xl border-2 p-4 font-bold shadow-npConvexCard ${themeColor} dark:shadow-darkConvexCard`}
+        className={`relative my-1 min-h-[320px] items-center whitespace-pre-wrap rounded-xl border-2 p-4 font-bold shadow-npConvexCard ${themeColor} dark:shadow-darkConvexCard`}
       >
-        <FaQuoteLeft className="absolute top-5 text-xl text-green-600"></FaQuoteLeft>
+        <FaQuoteLeft className="mt-4 text-xl text-green-600"></FaQuoteLeft>
         <div className="mt-12 text-base font-semibold italic tracking-wider text-slate-500 dark:text-slate-300">
           {quote}
 
-          <p className="font-base mt-12 text-sm tracking-wide text-slate-500 opacity-80 dark:text-slate-300">
+          <p className="font-base mt-16 text-sm tracking-wide text-slate-500 opacity-80 dark:text-slate-300">
             {`- ${source}`}
           </p>
         </div>
