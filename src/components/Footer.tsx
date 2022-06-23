554b97d5b59109d922cca36ce91dc60d98d52204
welove-t <tlove.tky@gmail.com>
2022-06-23 11:51:10 +0900

    ✨ 開発者リンクの設置

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <div className="mt-40 bg-gray-100 pt-10 pb-20">
-      <div className="container">
+      <div className="container text-center">
         <div className="flex justify-center space-x-4">
           <Link href="/terms">
             <a className="text-gray-600">利用規約</a>
@@ -13,12 +13,20 @@ const Footer = () => {
             <a className="text-gray-600">プライバシーポリシー</a>
           </Link>
         </div>
-        <div className="pt-4 text-center">
-          <Link href="/">
-            <a className="text-gray-600">当サービスの開発者</a>
-          </Link>
-          <p className="pt-4">&copy; QuoteCard</p>
-        </div>
+
+        <p className="mt-4 text-center">
+          このサービスは
+          <a
+            href="https://twitter.com/welove_tk"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline"
+          >
+            @welove_tk
+          </a>
+          が作成しました
+        </p>
+        <p className="pt-4">&copy; QuoteCard</p>
       </div>
     </div>
   );
