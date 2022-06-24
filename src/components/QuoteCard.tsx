732a103059dc19eb4def8eccb7efd94e0fa930bb
welove-t <tlove.tky@gmail.com>
2022-06-24 12:11:59 +0900

    💄 カードデザインの変更

--- src/components/QuoteCard.tsx
+++ src/components/QuoteCard.tsx
@@ -10,17 +10,21 @@ type props = {
 const QuoteCard = ({ themeColor, quote, source }: props) => {
   return (
     <div
-      className={`max-w-[360px] p-2 text-center sm:inline-block sm:w-[360px] ${themeColor}`}
+      className={`mx-auto max-w-[360px] px-2 pt-10 pb-1 text-center sm:inline-block sm:w-[360px] ${themeColor}`}
       id="canvas"
     >
       <div
-        className={`relative my-1 min-h-[320px] items-center whitespace-pre-wrap rounded-xl border-2 p-4 font-bold shadow-npConvexCard ${themeColor} dark:shadow-darkConvexCard`}
+        className={`relative my-1 flex min-h-[320px] items-center justify-center whitespace-pre-wrap rounded-xl border-2 p-4 font-bold shadow-npConvexCard ${themeColor} dark:shadow-darkConvexCard`}
       >
-        <FaQuoteLeft className="mt-4 text-xl text-green-600"></FaQuoteLeft>
+        <div
+          className={`${themeColor} absolute top-[-24px] flex h-12 w-12 items-center justify-center rounded-full border-2 shadow-npConvexCard dark:shadow-darkConvexCard sm:top-[-36px] sm:h-16 sm:w-16`}
+        >
+          <p className="my-auto text-2xl">Q</p>
+        </div>
         <div className="mt-12 text-base font-semibold italic tracking-wider text-slate-500 dark:text-slate-300">
           {quote}
 
-          <p className="font-base mt-16 text-sm tracking-wide text-slate-500 opacity-80 dark:text-slate-300">
+          <p className="font-base mt-16 mb-8 text-sm tracking-wide text-slate-500 opacity-80 dark:text-slate-300">
             {`- ${source}`}
           </p>
         </div>
