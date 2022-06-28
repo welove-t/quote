294f08997e7d52e71bae4ba399b88ab1896254f9
welove-t <tlove.tky@gmail.com>
2022-06-28 13:17:54 +0900

    💄 ヘッダーのスタイル調整

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 import { ColorTheme } from "src/components/ColorTheme";
 
@@ -8,10 +9,14 @@ type props = {
 export const Header = ({ themeColor }: props) => {
   return (
     <div
-      className={`fixed -inset-x-0 top-0 left-0 z-50 border-b border-solid  border-gray-200 ${themeColor} dark:border-gray-700`}
+      className={`fixed -inset-x-0 top-0 left-0 z-50 border-x-0 border-b-2 border-t-0 border-solid border-gray-200 ${themeColor} dark:border-gray-700`}
     >
       <header className="container flex h-16 items-center justify-center space-x-8">
-        <span className="text-2xl font-semibold text-gray-500">Quote Card</span>
+        <Link href="/">
+          <a className="text-2xl font-semibold text-gray-500 no-underline dark:text-gray-100">
+            Quote Card
+          </a>
+        </Link>
       </header>
       <ColorTheme />
     </div>
