fd3f274ee5e9dc922309e7f294cc22659b38269d
welove-t <tlove.tky@gmail.com>
2022-06-29 14:11:24 +0900

    ✨ 文字数上限オーバー時にボタン押下でメッセージ表示するように変更

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,20 +1,18 @@
 import React from "react";
 
 type props = {
-  isError: boolean;
   onClickButton: () => void;
   themeColor: string;
   children: React.ReactNode;
 };
 
-const Buttons = ({ isError, onClickButton, themeColor, children }: props) => {
+const Buttons = ({ onClickButton, themeColor, children }: props) => {
   return (
     <button
-      className={`mx-auto flex items-center justify-center space-x-2 rounded-full border-none px-8 py-2 font-semibold text-gray-400 shadow-npConvexButton hover:cursor-pointer hover:shadow-npConcaveButton disabled:cursor-not-allowed disabled:bg-gray-50 ${themeColor} dark:shadow-darkConvexButton dark:hover:shadow-darkConcaveButton`}
+      className={`mx-auto flex items-center justify-center space-x-2 rounded-full border-none px-8 py-2 font-semibold text-gray-400 shadow-npConvexButton hover:cursor-pointer hover:shadow-npConcaveButton ${themeColor} dark:shadow-darkConvexButton dark:hover:shadow-darkConcaveButton`}
       onClick={() => {
         onClickButton();
       }}
-      disabled={!isError}
     >
       {children}
     </button>
