98922b2c361dad357bcd466c7b2015143abefb46
welove-t <tlove.tky@gmail.com>
2022-07-22 12:03:03 +0900

    ✨ ロゴ追加

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,33 @@
-import Link from "next/link";
+import Image from "next/image";
 import React from "react";
 import { ColorTheme } from "src/components/ColorTheme";
+import { useMantineColorScheme } from "@mantine/core";
+import { useRouter } from "next/router";
 
 type props = {
   themeColor: string;
 };
 
 export const Header = ({ themeColor }: props) => {
+  const { colorScheme } = useMantineColorScheme();
+  const router = useRouter();
   return (
     <div
       className={`fixed -inset-x-0 top-0 left-0 z-50 border-x-0 border-b-2 border-t-0 border-solid border-gray-200 ${themeColor} dark:border-gray-700`}
     >
       <header className="container flex h-16 items-center justify-center space-x-8">
-        <Link href="/">
-          <a className="text-2xl font-semibold text-gray-600 no-underline dark:text-gray-100">
-            Quote Card
-          </a>
-        </Link>
+        <div className="w-40 cursor-pointer md:w-80">
+          {colorScheme && (
+            <Image
+              src={`/images/logo-quotecard-${colorScheme}.png`}
+              height={48}
+              width={320}
+              layout="responsive"
+              alt=""
+              onClick={() => router.push("/")}
+            ></Image>
+          )}
+        </div>
       </header>
       <ColorTheme />
     </div>
